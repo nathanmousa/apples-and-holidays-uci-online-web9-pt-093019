24d9c6d8b34471e0fe75d7aaf5efc71eb05832cc
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-19 18:25:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}"
+    puts "#{season.capitalize}:
     
     holiday.each do |holiday, supply|
       puts "#{holiday}: #{supply.flatten}"
