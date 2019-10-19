9a4fb35d57407221238e40cb8d817b97e8b0d840
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-19 18:42:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     holiday.each do |holiday, supply|
       new_holiday = []
-      holiday.to_s.split << new_holiday.capitalize
+      holiday.to_s.split << new_holiday
       
       puts "  #{new_holiday}: #{supply.join(", ")}"
     end
